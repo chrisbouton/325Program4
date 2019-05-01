e5edd122ccbd3e7bc452433d88fd60d8ad47d329
Chris <chrisbouton99@gmail.com>
2019-05-01 14:44:19 -0500

    started on classes

--- campusRecycle.py
+++ campusRecycle.py
@@ -3,3 +3,34 @@
 #Advanced Data Structures 325
 #Program 4: Campus Recycling
 
+class Vertex:
+    def __init__(self, data):
+        self.data = data
+        self.found = False
+
+    def getData(self):
+        return self.data
+
+    def setData(self, data):
+        self.data = data
+    
+
+class Edge:
+    def __init__(self, origin, dest, weight):
+        self.origin = origin
+        self.dest = dest
+        self.weight = weight
+    
+    def getOrigin(self):
+        return self.origin
+
+    def setOrigin(self, origin):
+        self.origin = origin
+
+    def getDest(self):
+        return self.dest
+    
+    def setDest(self, dest):
+        self.dest = dest
+
+
